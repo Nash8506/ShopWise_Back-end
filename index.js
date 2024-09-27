d9116975a581b049dfcce6e7a661941f7baa5d04
Nashid K <kp.nashid.nashid@gmail.com>
2024-09-27 17:59:16 +0530

    Added Mongoose connection options

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Database connection with MongoDB using .env file
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
